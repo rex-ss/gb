5a4417294e3a3ef5917bf3810425ab188f41d1bd
kangxiaodong <kangxiaodong@sayinfo.cn>
2019-10-21 11:35:14 +0800

    优化

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/panjf2000/ants/v2"
 	"github.com/urfave/cli"
 	"io"
 	"io/ioutil"
@@ -9,7 +10,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"sync"
+	"runtime"
 	"sync/atomic"
 	"time"
 )
@@ -19,22 +20,16 @@ var (
 	txMax = int64(0) //最大相应时长
 	txMin = int64(0) //最小相应时长
 
-	txTotal   = int64(0) //总请求
-	txSTotal  = int64(0) //成功请求
-	txETotal  = int64(0) //错误请求
-	reqsPool  = sync.Pool{}
-	reqCilent = sync.Pool{}
+	txTotal  = int64(0) //总请求
+	txSTotal = int64(0) //成功请求
+	txETotal = int64(0) //错误请求
+
+	client = &http.Client{}
 )
 
 func main() {
 
-	reqsPool.New = func() interface{} {
-		return &http.Request{}
-	}
-
-	reqCilent.New = func() interface{} {
-		return &http.Client{}
-	}
+	runtime.GOMAXPROCS(runtime.NumCPU() * 2)
 
 	app := &cli.App{
 		Name: "glr",
@@ -47,6 +42,7 @@ func main() {
 		},
 	}
 	app.Run(os.Args)
+
 }
 
 var TimeLimit = cli.Command{
@@ -91,11 +87,9 @@ var TimeLimit = cli.Command{
 
 func timeLimit(c *cli.Context) {
 
-	tm := time.Now()
-
 	ts := c.Int("limit")
 	ur := c.String("url")
-	m := string("m")
+	m := c.String("m")
 	T := c.String("T")
 	p := c.String("p")
 	concurrency := c.Int("c")
@@ -115,68 +109,67 @@ func timeLimit(c *cli.Context) {
 	if err != nil {
 		log.Fatal("url.Parse error:", err.Error())
 	}
-	once := sync.Once{}
+
 	ta := time.After(time.Duration(int64(ts)) * time.Second)
-	for {
-		select {
-		case <-ta:
-			goto end
-		default:
-			wg := sync.WaitGroup{}
+	req1 := &http.Request{}
 
-			wg.Add(concurrency)
-			for i := 0; i < concurrency; i++ {
-				go func() {
-					defer func() {
-						if r := recover(); r != nil {
-							log.Println("timeLimit recover:", r)
-						}
-					}()
+	req1.Method = m
+	req1.URL = u
 
-					client := &http.Client{}
-					req := reqsPool.Get().(*http.Request)
-					req.Method = m
-					req.URL = u
-
-					req.Header = make(map[string][]string)
-					req.Header.Set("Content-Type", "text/plain")
-					if T != "" {
-						req.Header.Set("Content-Type", T)
-					}
-					if p != "" {
-						req.Body = bodyReader
-					}
-
-					tm := time.Now()
-					resp, err := client.Do(req)
-
-					since := int64(time.Since(tm))
-					once.Do(func() { //对 min 进行一次初始化
-						atomic.SwapInt64(&txMin, since)
-					})
-					if txMax < since {
-						atomic.SwapInt64(&txMax, since)
-					} else if txMin > since {
-						atomic.SwapInt64(&txMin, since)
-					}
-
-					atomic.AddInt64(&txAvg, since)
-					atomic.AddInt64(&txTotal, 1)
-					if err != nil {
-						atomic.AddInt64(&txETotal, 1)
-					} else {
-						if resp.StatusCode == 200 {
-							atomic.AddInt64(&txSTotal, 1)
-						}
-					}
-					wg.Done()
-				}()
-			}
-			wg.Wait()
-		}
+	req1.Header = make(map[string][]string)
+	req1.Header.Set("Content-Type", "text/plain")
+	req1.Header.Set("Connection", "keep-alive")
+	if T != "" {
+		req1.Header.Set("Content-Type", T)
 	}
-end:
-	log.Println("since:", time.Since(tm))
-	fmt.Println(fmt.Sprintf("txTotal:\t%d\tSuccess:\t%d\tError:\t%d\ttx/s:%.2f", txTotal, txSTotal, txETotal, float64(txSTotal)/float64(ts)))
-	fmt.Println(fmt.Sprintf("avg:\t%s\ttMax\t%s\ttMin\t%s", time.Duration(txAvg/txTotal).String(), time.Duration(txMax).String(), time.Duration(txMin).String()))
+	if p != "" {
+		req1.Body = bodyReader
+	}
+	tm := time.Now()
+	client.Do(req1)
+	since := int64(time.Since(tm))
+	atomic.SwapInt64(&txMin, since)
+	pool, _ := ants.NewPool(10000)
+	defer pool.Release()
+	for i := 0; i < concurrency; i++ {
+		go func() {
+			req := &http.Request{}
+			req.Method = m
+			req.URL = u
+			req.Header = make(map[string][]string)
+			req.Header.Set("Content-Type", "text/plain")
+			req.Header.Set("Connection", "keep-alive")
+			if T != "" {
+				req.Header.Set("Content-Type", T)
+			}
+			if p != "" {
+				req.Body = bodyReader
+			}
+			for {
+				tm := time.Now()
+				resp, err := client.Do(req)
+				since := int64(time.Since(tm))
+				atomic.AddInt64(&txAvg, since)
+				if txMax < since {
+					atomic.SwapInt64(&txMax, since)
+				} else if txMin > since {
+					atomic.SwapInt64(&txMin, since)
+				}
+
+				if err == nil {
+					if resp.StatusCode == 200 {
+						atomic.AddInt64(&txSTotal, 1)
+					}
+				} else {
+					fmt.Println("err:", err.Error())
+					atomic.AddInt64(&txETotal, 1)
+				}
+			}
+		}()
+
+	}
+	<-ta
+	log.Println("go number:", runtime.NumGoroutine())
+	fmt.Println(fmt.Sprintf("txTotal:\t%d\tSuccess:\t%d\tError:\t%d\ttx/s:%.2f", txSTotal+txETotal, txSTotal, txETotal, float64(txSTotal)/float64(ts)))
+	fmt.Println(fmt.Sprintf("avg:\t%s\ttMax\t%s\ttMin\t%s", time.Duration(txAvg/(txSTotal+txETotal)), time.Duration(txMax).String(), time.Duration(txMin).String()))
 }
